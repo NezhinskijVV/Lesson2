75df10e909ee1ca13ed0f59ca9f814e1a09c9007
Vitalij Nezhinskij <SeekerOfConfessor@gmail.com>
2019-10-08 01:07:34 +0300

    lesson 5

--- src/ru/itsjava/algorithms/Recursion.java
+++ src/ru/itsjava/algorithms/Recursion.java
@@ -11,7 +11,9 @@ public class Recursion {
 
     //5! = 5 * 4 * 3 * 2 * 1
     public static int factorial(int num) {
-        if (num == 1) return 1;
+        if ((num == 0) || (num == 1)) {
+            return 1;
+        }
         return num * factorial(num - 1);
     }
 
