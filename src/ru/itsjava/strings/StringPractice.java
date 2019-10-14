554ff8c675f6cd9dec82bc205f0d643deb9add8f
Vitalij Nezhinskij <SeekerOfConfessor@gmail.com>
2019-10-15 00:42:54 +0300

    lesson 7 String Practice

---
+++ src/ru/itsjava/strings/StringPractice.java
@@ -0,0 +1,14 @@
+package ru.itsjava.strings;
+
+/**
+ * Created by Nezhinskij VV on 14.10.2019.
+ */
+public class StringPractice {
+    public static void main(String[] args) {
+        String spell = "Экспелиармус!";
+        System.out.println(spell);
+        System.out.println(spell.length());
+        System.out.println(spell.contains("ели"));
+
+    }
+}
